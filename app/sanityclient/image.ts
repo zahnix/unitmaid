dda2a5ac0bc489f464fc75828a33d57ac2c6cb4a
Baqir Naqvi <b.naqvi99@gmail.com>
2023-05-29 03:44:12 +0500

    Vercel Deployment Fix

--- app/sanityclient/image.ts
+++ app/sanityclient/image.ts
@@ -1,8 +1,8 @@
-import imageURL from '@sanity/image-url'
+import imageURL from "@sanity/image-url";
 
 export const imgURL = (source: any) => {
-    return imageURL({
-      projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-      dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-    }).image(source);
-}
+  return imageURL({
+    projectId: "unitmaid",
+    dataset: "unitmaid",
+  }).image(source);
+};
