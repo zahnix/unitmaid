3655d5e899d0ae26a108c6b44902c85e623689d7
Cronenberg Code <abzahui@gmail.com>
2023-06-02 18:01:34 -0400

    remove auth pages

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -63,31 +63,14 @@ export default function Navbar() {
           </Flex>
         </Flex>
         <Stack
-          flex={{ base: 1, md: 0 }}
+          flex={{ base: 1, md: 1 }}
           justify={'flex-end'}
           direction={'row'}
-          spacing={6}>
-          <Button
-            as={'a'}
-            fontSize={'sm'}
-            fontWeight={400}
-            variant={'link'}
-            href={'signin'}>
-            Sign In
-          </Button>
-          <Button
-            as={'a'}
-            display={{ base: 'none', md: 'inline-flex' }}
-            fontSize={'sm'}
-            fontWeight={600}
-            color={'white'}
-            bg={'gray.400'}
-            href={'signup'}
-            _hover={{
-              bg: 'green.300',
-            }}>
-            Sign Up
-          </Button>
+          >
+          <Text fontSize={'md'} fontWeight={'bold'} color={'green.500'} as='u'>
+            <PhoneIcon />
+            <a color='green' href={`tel:${"703-424-2905"}`}>CALL US</a>
+          </Text>
         </Stack>
       </Flex>
 
