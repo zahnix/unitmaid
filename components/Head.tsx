e95296c016f0fe35668af233df4e50e6104930c9
Cronenberg Code <abzahui@gmail.com>
2023-06-06 09:31:27 -0400

    seo/responsiveness

--- components/Head.tsx
+++ components/Head.tsx
@@ -2,6 +2,7 @@ export default function Head() {
   return(
     <>
         <title>UnitMaid | Uncluttered Storage</title>
+        <meta content='width=device-width, initial-scale=1' name='viewport'/>
         <meta property='og:description' content='UnitMaid is a professional storage unit management company that provides customizable, affordable, and comprehensive storage management solutions such as storage unit cleaning, organization, relocation, consignment, pick-up service and more. Let us take optimize and care of your storage needs by maximizing space, enhancing accessibility, and ensuring proper maintenance. Get a Quote today' key='description'/>
     </>
   )
